86f6c57c1c61772d7d20d5c3fdffe5d837699cb5
ayoubzoubeidi <ayoubzoubeidi@gmail.com>
2020-08-03 13:53:50 +0100

    Implemented BeerController services, closes #15

---
+++ src/main/java/com/maz/msscbeerservice/services/BeerService.java
@@ -0,0 +1,13 @@
+package com.maz.msscbeerservice.services;
+
+import com.maz.msscbeerservice.web.model.BeerDto;
+
+import java.util.UUID;
+
+public interface BeerService {
+    BeerDto getById(UUID beerId);
+
+    BeerDto saveNewBeer(BeerDto beerDto);
+
+    BeerDto updateBeer(UUID beerId, BeerDto beerDto);
+}
---
+++ src/main/java/com/maz/msscbeerservice/services/BeerServiceImpl.java
@@ -0,0 +1,42 @@
+package com.maz.msscbeerservice.services;
+
+import com.maz.msscbeerservice.domain.Beer;
+import com.maz.msscbeerservice.repositories.BeerRepository;
+import com.maz.msscbeerservice.web.controller.NotFoundException;
+import com.maz.msscbeerservice.web.mappers.BeerMapper;
+import com.maz.msscbeerservice.web.model.BeerDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@RequiredArgsConstructor
+@Service
+public class BeerServiceImpl implements BeerService {
+
+    private final BeerRepository beerRepository;
+
+    private final BeerMapper beerMapper;
+
+    @Override
+    public BeerDto getById(UUID beerId) {
+        return beerMapper.beerToBeerDto(beerRepository.findById(beerId).orElseThrow(NotFoundException::new));
+    }
+
+    @Override
+    public BeerDto saveNewBeer(BeerDto beerDto) {
+        return beerMapper.beerToBeerDto(beerRepository.save(beerMapper.beerDtoToBeer(beerDto)));
+    }
+
+    @Override
+    public BeerDto updateBeer(UUID beerId, BeerDto beerDto) {
+
+        Beer beer = beerRepository.findById(beerId).orElseThrow(NotFoundException::new);
+        beer.setBeerName(beerDto.getBeerName());
+        beer.setBeerStyle(beerDto.getBeerStyle().name());
+        beer.setPrice(beerDto.getPrice());
+        beer.setUpc(beerDto.getUpc());
+
+        return beerMapper.beerToBeerDto(beerRepository.save(beer));
+    }
+}
