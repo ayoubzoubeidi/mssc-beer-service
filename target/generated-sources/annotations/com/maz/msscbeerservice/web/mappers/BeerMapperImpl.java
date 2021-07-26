7ba62d7b129dc88e26d2aea4bba14d7f33faa3cf
ayoub <ayoubzoubeidi@gmail.com>
2021-07-26 15:28:00 +0100

    Updated mapstruct version after bug on the version used. TO 1.4.2.FINAL

---
+++ target/generated-sources/annotations/com/maz/msscbeerservice/web/mappers/BeerMapperImpl.java
@@ -0,0 +1,33 @@
+package com.maz.msscbeerservice.web.mappers;
+
+import com.maz.brewery.model.BeerDto;
+import com.maz.msscbeerservice.domain.Beer;
+import javax.annotation.processing.Generated;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2021-07-26T15:24:40+0100",
+    comments = "version: 1.4.2.Final, compiler: javac, environment: Java 11.0.11 (Amazon.com Inc.)"
+)
+@Component
+@Primary
+public class BeerMapperImpl extends BeerMapperDecorator implements BeerMapper {
+
+    @Autowired
+    @Qualifier("delegate")
+    private BeerMapper delegate;
+
+    @Override
+    public Beer beerDtoToBeer(BeerDto beerDto)  {
+        return delegate.beerDtoToBeer( beerDto );
+    }
+
+    @Override
+    public BeerDto beerDtoToBeerWithoutInventory(Beer beer)  {
+        return delegate.beerDtoToBeerWithoutInventory( beer );
+    }
+}
---
+++ target/generated-sources/annotations/com/maz/msscbeerservice/web/mappers/BeerMapperImpl_.java
@@ -0,0 +1,94 @@
+package com.maz.msscbeerservice.web.mappers;
+
+import com.maz.brewery.model.BeerDto;
+import com.maz.brewery.model.BeerStyleEnum;
+import com.maz.msscbeerservice.domain.Beer;
+import javax.annotation.processing.Generated;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2021-07-26T15:24:40+0100",
+    comments = "version: 1.4.2.Final, compiler: javac, environment: Java 11.0.11 (Amazon.com Inc.)"
+)
+@Component
+@Qualifier("delegate")
+public class BeerMapperImpl_ implements BeerMapper {
+
+    @Autowired
+    private DateMapper dateMapper;
+
+    @Override
+    public Beer beerDtoToBeer(BeerDto beerDto) {
+        if ( beerDto == null ) {
+            return null;
+        }
+
+        Beer beer = new Beer();
+
+        beer.setId( beerDto.getId() );
+        if ( beerDto.getVersion() != null ) {
+            beer.setVersion( beerDto.getVersion().longValue() );
+        }
+        beer.setCreatedDate( dateMapper.asTimeStamp( beerDto.getCreatedDate() ) );
+        beer.setLastModifiedDate( dateMapper.asTimeStamp( beerDto.getLastModifiedDate() ) );
+        beer.setBeerName( beerDto.getBeerName() );
+        if ( beerDto.getBeerStyle() != null ) {
+            beer.setBeerStyle( beerDto.getBeerStyle().name() );
+        }
+        beer.setUpc( beerDto.getUpc() );
+        beer.setPrice( beerDto.getPrice() );
+
+        return beer;
+    }
+
+    @Override
+    public BeerDto beerToBeerDto(Beer beer) {
+        if ( beer == null ) {
+            return null;
+        }
+
+        BeerDto beerDto = new BeerDto();
+
+        beerDto.setId( beer.getId() );
+        if ( beer.getVersion() != null ) {
+            beerDto.setVersion( beer.getVersion().intValue() );
+        }
+        beerDto.setCreatedDate( dateMapper.asOffsetDateTime( beer.getCreatedDate() ) );
+        beerDto.setLastModifiedDate( dateMapper.asOffsetDateTime( beer.getLastModifiedDate() ) );
+        beerDto.setBeerName( beer.getBeerName() );
+        if ( beer.getBeerStyle() != null ) {
+            beerDto.setBeerStyle( Enum.valueOf( BeerStyleEnum.class, beer.getBeerStyle() ) );
+        }
+        beerDto.setUpc( beer.getUpc() );
+        beerDto.setPrice( beer.getPrice() );
+
+        return beerDto;
+    }
+
+    @Override
+    public BeerDto beerDtoToBeerWithoutInventory(Beer beer) {
+        if ( beer == null ) {
+            return null;
+        }
+
+        BeerDto beerDto = new BeerDto();
+
+        beerDto.setId( beer.getId() );
+        if ( beer.getVersion() != null ) {
+            beerDto.setVersion( beer.getVersion().intValue() );
+        }
+        beerDto.setCreatedDate( dateMapper.asOffsetDateTime( beer.getCreatedDate() ) );
+        beerDto.setLastModifiedDate( dateMapper.asOffsetDateTime( beer.getLastModifiedDate() ) );
+        beerDto.setBeerName( beer.getBeerName() );
+        if ( beer.getBeerStyle() != null ) {
+            beerDto.setBeerStyle( Enum.valueOf( BeerStyleEnum.class, beer.getBeerStyle() ) );
+        }
+        beerDto.setUpc( beer.getUpc() );
+        beerDto.setPrice( beer.getPrice() );
+
+        return beerDto;
+    }
+}
