b3c21e0fd4932b963ca8030fb34edf1531b0553a
ayoub <ayoubzoubeidi@gmail.com>
2021-07-30 10:23:23 +0100

    Added spring cloud server config using legacy bootstrap method.

--- target/generated-sources/annotations/com/maz/msscbeerservice/web/mappers/BeerMapperImpl.java
+++ target/generated-sources/annotations/com/maz/msscbeerservice/web/mappers/BeerMapperImpl.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2021-07-28T12:29:39+0100",
+    date = "2021-07-30T10:21:02+0100",
     comments = "version: 1.4.2.Final, compiler: javac, environment: Java 11.0.11 (Amazon.com Inc.)"
 )
 @Component
